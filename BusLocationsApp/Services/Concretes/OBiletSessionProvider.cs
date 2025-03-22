dae16aba1b5e69705fd7a9263c6e8d13a4fa02e6
Gokhan <Gokhan@DESKTOP-V5E1RUI>
2025-03-22 18:14:51 +0300

    refactor(OBiletSessionProvider): comments added

--- BusLocationsApp/Services/Concretes/OBiletSessionProvider.cs
+++ BusLocationsApp/Services/Concretes/OBiletSessionProvider.cs
@@ -2,14 +2,29 @@
 
 namespace BusLocationsApp.Services.Concretes
 {
+    /// <summary>
+    /// Provides functionality to manage and retrieve sessions.
+    /// Combines session retrieval logic and storage to ensure
+    /// consistent and reusable session management.
+    /// </summary>
     public class OBiletSessionProvider(IOBiletSessionManager oBiletSessionManager, IOBiletSessionClient oBiletSessionClient) : IOBiletSessionProvider
     {
+        /// <summary>
+        /// Retrieves an existing session or creates a new one if none is available.
+        /// </summary>
+        /// <returns>
+        /// A tuple containing the session ID and device ID. 
+        /// If a session doesn't exist, a new session is fetched from the OBilet session API and stored locally.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">Thrown if a new session cannot be fetched from the API.</exception>
         public async Task<(string SessionId, string DeviceId)> GetOrCreateSessionAsync()
         {
+            // Retrieve the current session from the session manager.
             var (sessionId, deviceId) = oBiletSessionManager.GetSession();
 
-            if (string.IsNullOrEmpty(sessionId) || string.IsNullOrEmpty(deviceId))
+            if (CheckSessionData(sessionId, deviceId))
             {
+                // Fetch a new session from the session API.
                 var newSession = await oBiletSessionClient.GetSessionAsync(new Models.SessionRequest
                 {
                     //todo:tarayıcı bilgileri httpcontext den alınabilir
@@ -33,14 +48,27 @@ namespace BusLocationsApp.Services.Concretes
 
                 if (newSession != null)
                 {
+                    // Update the session and device IDs with the new data.
                     sessionId = newSession.data.sessionId;
                     deviceId = newSession.data.deviceId;
 
+                    // Save the new session data locally for future use.
                     oBiletSessionManager.SaveSession(sessionId, deviceId);
+                }
+                else
+                {
+                    // Handle cases where the API fails to return a valid session.
+                    throw new InvalidOperationException("Unable to fetch a new session from the API.");
                 }
             }
 
+            // Return the existing or newly created session data.
             return (sessionId!, deviceId!);
+        }
+
+        private static bool CheckSessionData(string? sessionId, string? deviceId)
+        {
+            return string.IsNullOrEmpty(sessionId) || string.IsNullOrEmpty(deviceId);
         }
     }
 }
